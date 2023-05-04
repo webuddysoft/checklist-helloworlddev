f247ba48b1277a26cdc1542e9f11d45d59b0c9e2
softdev <softdev1114@gmail.com>
2023-05-04 15:21:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() {  
+  const [checklist, setCheckList] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const savedList = localStorage.getItem('checklist');
+    setCheckList(!savedList ? [] : JSON.parse(savedList));
+  }, []);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.key === 'Enter') {
+      setCheckList((prev) => {
+        setInput('');
+        const newList = prev.concat([e.target.value]);
+        localStorage.setItem('checklist', JSON.stringify(newList));
+        return newList;
+      });
+    }
+  }
+
+  const handleCheckItem = (idx) => {
+    setCheckList((prev) => {
+      const newList = prev.filter((item, i) => i != idx);
+      localStorage.setItem('checklist', JSON.stringify(newList));
+      return newList;
+    })
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Checklist
       </header>
+      <div className="input-box">
+        <label htmlFor='new-check'>Add New Item</label>
+        <input type="text" id='new-check' value={input} onChange={handleInput} onKeyPress={handleEnterKey} />
+      </div>
+      <div className="check-list">
+        {checklist.length === 0 
+          ? <div className='check-item'>No data found</div>
+          : (checklist.map((item, index) => {
+              return (
+                <div className='check-item' key={`check-item${index}`}>
+                  <input type="checkbox" checked={false} onChange={() => handleCheckItem(index)}  />
+                  {item}
+                </div>
+              )
+            })
+          )}
+      </div>
     </div>
   );
 }
